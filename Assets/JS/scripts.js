f40f0d803694f24b3949e37ecb77e4746375b481
Aaron Allen <aaronseth.allen@gmail.com>
2024-07-16 15:42:32 -0400

    Updated weather API, collected data, and began formatting output.

--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -1,43 +1,78 @@
 const API_KEY = '86220d499e0b202925c9d46140b3fcaa';
-
-
+const currentSection = document.getElementById('currentSection');
+const forecastSection = document.getElementById('forecastSection');
 const citySearchForm = document.getElementById('citySearchForm');
+const archiveList = document.getElementById('archiveList');
 
-//`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${API_KEY}`
 
-
-http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-function getLatLong(event){
+async function getAllWeather (event){
     event.preventDefault();
+    let cityReq;
+    if(document.getElementById('selectedCity').value.length > 0 ){
+        cityReq =  document.getElementById('selectedCity').value;
+    }else if(event.currentTarget.textContent.trim() != 'Search'){
+        cityReq  = event.currentTarget.textContent.trim();
+    }else{
+        alert('Entry invalid!');
+        return;
+    }
 
-    const cityQuery = document.getElementById('selectedCity').value;
-    const requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityQuery}&limit=3&appid=${API_KEY}`;
+    const currentUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityReq}&appid=${API_KEY}`;
+    const forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${cityReq}&appid=${API_KEY}`;
 
 
-    fetch(requestUrl)
-        .then((response)=>{
-            console.log(response);
-            return response.json();
-        })
-        .then((data)=>{
-            console.log(data);
-            getWeather(data[0].lat, data[0].lon);
-        });
+
+    const forecastData =[{
+        date: "mm/dd/yyyy",
+        temp: "deg",
+        wind: 0,
+        weather: 'clear',
+        humidity: 0,
+    }];
+
+
+
+    let response = await fetch(currentUrl).then((response)=>response.json());
+    forecastData[0]={
+        date: new Date(response.dt * 1000).toLocaleDateString(),
+        temp: ((response.main.temp - 273.15) * (9/5) + 32).toFixed(2)+' \u00B0F',
+        wind: response.wind.speed,
+        weather: response.weather[0].main,
+        humidity: response.main.humidity
+    }
+
+    response = await fetch(forecastUrl).then((response)=>response.json());
+    //console.log(response);
+    let n = 1;
+    for(i=4; i<response.list.length ; i+=8){
+        forecastData[n]={
+            date: new Date(response.list[i].dt * 1000).toLocaleDateString(),
+            temp: ((response.list[i].main.temp - 273.15) * (9/5) + 32).toFixed(2)+' \u00B0F',
+            wind: response.list[i].wind.speed,
+            weather: response.list[i].weather[0].main,
+            humidity: response.list[i].main.humidity
+        }
+        n+=1;
+    }
+
+    console.log(forecastData);
+
+    displayOutput(cityReq, forecastData);
+    addToArchive(cityReq);
+
 }
 
-function getWeather(lat, long){
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${API_KEY}`;
 
-    fetch(apiUrl)
-        .then((response)=>{
-            console.log(response);
-            return response.json();
-        })
-        .then((data)=>{
-            console.log(data);
-        });
+function displayOutput(city, data){
+    const testCase = `<h1>${city}</h2>`;
+    
+    currentSection.innerHTML = testCase;
+    
+
+
+
 }
 
-
-
-citySearchForm.addEventListener('submit', getLatLong);
+window.addEventListener('load',()=>{
+    citySearchForm.addEventListener('submit', getAllWeather);
+});
